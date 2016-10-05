e668bc90a9c1d691e3555ef61d26acc9db6a5279
wladislawbucko <studing1995@gmail.com>
2016-10-05 13:17:54 +0300

    first download of project

---
+++ bin/custom/cuppytrail/gensrc/de/hybris/platform/cuppytrail/constants/GeneratedCuppytrailConstants.java
@@ -0,0 +1,66 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Oct 4, 2016 12:54:13 AM                     ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ *  
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *  
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *  
+ */
+package de.hybris.platform.cuppytrail.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCuppytrailConstants
+{
+	public static final String EXTENSIONNAME = "cuppytrail";
+	public static class TC
+	{
+		public static final String STADIUM = "Stadium".intern();
+		public static final String STADIUMACCESS = "StadiumAccess".intern();
+		public static final String STADIUMTYPE = "StadiumType".intern();
+	}
+	public static class Attributes
+	{
+		public static class Match
+		{
+			public static final String STADIUM = "stadium".intern();
+		}
+	}
+	public static class Enumerations
+	{
+		public static class StadiumAccess
+		{
+			public static final String ROAD = "road".intern();
+			public static final String RAIL = "rail".intern();
+			public static final String PLANE = "plane".intern();
+		}
+		public static class StadiumType
+		{
+			public static final String OPENAIR = "openair".intern();
+			public static final String ENCLOSED = "enclosed".intern();
+		}
+	}
+	public static class Relations
+	{
+		public static final String STADIUMMATCHRELATION = "StadiumMatchRelation".intern();
+	}
+	
+	protected GeneratedCuppytrailConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
