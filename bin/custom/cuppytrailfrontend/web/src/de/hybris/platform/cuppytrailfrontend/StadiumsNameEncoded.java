9627c2d267c827607957cd8f660bc5d610800d30
wladislawbucko <studing1995@gmail.com>
2016-10-07 01:30:16 +0300

    added new module. set up servlet and encoded class

--- bin/custom/cuppytrailfrontend/web/src/de/hybris/platform/cuppytrailfrontend/StadiumsNameEncoded.java
+++ bin/custom/cuppytrailfrontend/web/src/de/hybris/platform/cuppytrailfrontend/StadiumsNameEncoded.java
@@ -1,7 +1,24 @@
 package de.hybris.platform.cuppytrailfrontend;
 
-/**
- * Created by wladek on 10/7/16.
- */
-public class StadiumsNameEncoded {
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.apache.log4j.Logger;
+
+public class StadiumsNameEncoded
+{
+    private final static Logger LOG = Logger.getLogger(StadiumsNameEncoded.class);
+
+    public static String getNameEncoded(final String name)
+    {
+        try
+        {
+            return URLEncoder.encode(name, "UTF-8");
+        }
+        catch (final UnsupportedEncodingException e)
+        {
+            LOG.error("Problems while encoding", e);
+            return "";
+        }
+    }
 }
