3fc5d3879ffdd5d8f34b6e1280806bcf58d11c67
wladislawbucko <studing1995@gmail.com>
2016-10-12 09:55:17 +0300

    finished cockpit_basics

---
+++ bin/custom/cuppytrailcockpit/gensrc/de/hybris/platform/cuppytrailcockpit/constants/GeneratedCuppytrailcockpitConstants.java
@@ -0,0 +1,35 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Oct 12, 2016 12:46:05 AM                    ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ *  
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *  
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *  
+ */
+package de.hybris.platform.cuppytrailcockpit.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCuppytrailcockpitConstants
+{
+	public static final String EXTENSIONNAME = "cuppytrailcockpit";
+	
+	protected GeneratedCuppytrailcockpitConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
