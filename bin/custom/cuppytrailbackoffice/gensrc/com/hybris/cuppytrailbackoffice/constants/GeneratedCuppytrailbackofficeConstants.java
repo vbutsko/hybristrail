938a87e6678df2b7b85c07c8f46db1ac2b27d92e
wladislawbucko <studing1995@gmail.com>
2016-10-17 09:50:24 +0300

    some tries of backoffice

---
+++ bin/custom/cuppytrailbackoffice/gensrc/com/hybris/cuppytrailbackoffice/constants/GeneratedCuppytrailbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Oct 16, 2016 9:29:26 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.hybris.cuppytrailbackoffice.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCuppytrailbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "cuppytrailbackoffice";
+	
+	protected GeneratedCuppytrailbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
